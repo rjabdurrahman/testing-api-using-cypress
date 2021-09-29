aef6c263f6251ffecf1ad78593d513ff15e68b42
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-09-29 15:56:20 +0600

    Added: staus and file detials checking on file upload testing

--- cypress/integration/anonfiles API Testing/fileUpload.spec.js
+++ cypress/integration/anonfiles API Testing/fileUpload.spec.js
@@ -1,26 +1,53 @@
+// Already have a baseUrl in cypress.json
+const baseUrlOfAnonFiles = 'https://api.anonfiles.com';
+
 before(() => {
     /* Visiting this site to Bypass CORS for different Base URL */
-    cy.visit('https://api.anonfiles.com')
+    cy.visit(baseUrlOfAnonFiles)
 })
 
 describe('Testing file Upload', () => {
-    it('Should send response 200', () => {
+    it('Should send status 400 if don\'t attach file', () => {
+        cy.request({
+            method: 'POST',
+            url: `${baseUrlOfAnonFiles}/upload`,
+            failOnStatusCode: false
+        })
+            .then(response => {
+                expect(response.status).to.be.eq(400)
+            })
+    })
+
+    it('Should send status 200 on file upload', () => {
         const fileName = 'demo_file.txt';
-        const url = 'https://api.anonfiles.com/upload';
+        const url = `${baseUrlOfAnonFiles}/upload`;
         const fileType = 'text/plain';
         cy.fixture(fileName, 'binary').then((fileBin) => {
             const blob = Cypress.Blob.binaryStringToBlob(fileBin, fileType);
             const formData = new FormData();
             formData.set('file', blob, fileName);
             cy.form_request(url, formData)
-            .then(result => {
-                let response = result.response;
-                expect(result.status).to.eq(200);
-                expect(response.body).to.be.a('object');
-                expect(response.body.data).to.have.property('file');
-                expect(response.body.data.file.metadata.name).to.be.equal(fileName);
-            })
+                .then(result => {
+                    expect(result.status).to.eq(200);
+                })
         })
+    })
 
+    it('Should get file details on response', () => {
+        const fileName = 'demo_file.txt';
+        const url = `${baseUrlOfAnonFiles}/upload`;
+        const fileType = 'text/plain';
+        cy.fixture(fileName, 'binary').then((fileBin) => {
+            const blob = Cypress.Blob.binaryStringToBlob(fileBin, fileType);
+            const formData = new FormData();
+            formData.set('file', blob, fileName);
+            cy.form_request(url, formData)
+                .then(result => {
+                    let response = result.response;
+                    expect(response.body).to.be.a('object');
+                    expect(response.body.data).to.have.property('file');
+                    expect(response.body.data.file.metadata.name).to.be.equal(fileName);
+                })
+        })
     })
 })

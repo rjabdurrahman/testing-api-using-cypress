1d1746b416ef89eb1a84287337af7a69f9ae35cf
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-09-29 12:44:00 +0600

    Added: /symbols response with valid api key

--- cypress/integration/Quote API Testing/symbols.spec.js
+++ cypress/integration/Quote API Testing/symbols.spec.js
@@ -1,4 +1,5 @@
 describe('Endpoint GET /symbols', () => {
+    const apiKey = Cypress.env('API_KEY')
 
     it('Should send status 200', () => {
         cy.request('/symbols')
@@ -15,6 +16,14 @@ describe('Endpoint GET /symbols', () => {
                 expect(body).to.have.keys('error', 'message')
                 expect(body).to.have.property('error', true)
                 expect(body).to.have.property('message', "API Key Not Valid. Please go to 1forge.com to get an API key. If you have any questions please email us at contact@1forge.com")
+            })
+    })
+
+    it('Request with valid API Key should send an array', () => {
+        cy.request(`/symbols?api_key=${apiKey}`)
+            .then(response => {
+                let body = response.body;
+                expect(body).to.a('array')
             })
     })
 
